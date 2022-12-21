1e41379946039a1838d24dddbbb66fe13695df10
Pranay Sood <pranay.sd@gmail.com>
2022-12-21 23:46:44 +0800

    add account class

--- src/main/model/account/Account.java
+++ src/main/model/account/Account.java
@@ -1,7 +1,40 @@
 package src.main.model.account;
 
+import java.util.HashMap;
+
+import src.main.model.Trade.Stock;
+
 public abstract class Account {
+    private HashMap<Stock, Integer> portfolio;
+    private double funds;
 
+    public Account(double funds) {
+        this.funds = funds;
+        this.portfolio = new HashMap<Stock, Integer>();
+        this.portfolio.put(Stock.AAPL, 0);
+        this.portfolio.put(Stock.FB, 0);
+        this.portfolio.put(Stock.GOOG, 0);
+        this.portfolio.put(Stock.TSLA, 0);
+    }
 
+    public Account(Account source) {
+        this.funds = source.funds;
+    }
+
+    public double getFunds() {
+        return this.funds;
+    }
+
+    public void setFunds(double funds) {
+        this.funds = funds;
+    }
+
+    public Integer getShares(Stock stock) {
+        return this.portfolio.get(stock);
+    }
+
+    public void setShares(Stock stock, Integer shares) {
+        this.portfolio.put(stock, shares);
+    }
 
 }
